88ff8e2204042f73af9ec39f3c7781a3bb4b2622
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-02-18 12:34:44 +0100

    Question Dialog Improvement

--- ScribbleJam/Assets/Scripts/QuestionScript.cs
+++ ScribbleJam/Assets/Scripts/QuestionScript.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionScript : MonoBehaviour
 {
-
+    public Text questionText;
+    public string newQuestion;
+    public GameObject rightAnswer;
+    public GameObject[] wrongAnswers;
     public void RightAnswer()
     {
         PlayerStats playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
 
         playerStats.playerHP++;
         playerStats.keyFragments++;
+
+        Destroy(rightAnswer);
+        for(int i = 0; i < wrongAnswers.Length; i++)
+        {
+            Destroy(wrongAnswers[i]);
+        }
+
+        questionText.text = newQuestion;
         
     }
 
